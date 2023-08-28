c6887a7ed70cea4a17fa398f665fa03d4fb1a0c7
DDVENKAT <ddvenkat09@gmail.com>
2023-08-28 21:44:31 +0530

    php-files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP App</title>
+</head>
+<body>
+    <h1>Hello, World! php-application</h1>
+    <p>This is a simple PHP application.</p>
+</body>
+</html>
+
